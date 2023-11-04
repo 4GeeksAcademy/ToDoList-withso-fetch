721d0c45e3705b7de3e16d363fe32b796d974821
Adrian <142854436+withyso@users.noreply.github.com>
2023-11-04 14:29:31 +0000

    ajustes a la tarea

--- src/js/component/TaskTodo.jsx
+++ src/js/component/TaskTodo.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { useState } from "react";
 
 export const InputData = (content, SetNewTask) => {
-    const [inputData, SetInputData] = useState(['']);
+    const [inputData, SetInputData] = useState('');
+    const [liContent, setliContent] = useState(['']);
 
     return (
         <div className="">
-            <form action="full-form">
+            <form onSubmit ={ (event) => {
+                setliContent(inputData);
+                event.preventDefault();
+                console.log(liContent);
+            }} action="full-form">
                 <input onChange = { (event) => {
                     SetInputData(event.target.value);
                     console.log(inputData);
-                    
                 }}
                  type ="text"
                  placeholder ="Whats needs to be done" />
+                 	<li> {liContent} </li>
+					<li> {liContent} </li>
+					<li> {liContent} </li>
+					<li> {liContent} </li>	
             </form>
         </div>
     );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,18 @@ import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-import { InputData } from "./InputData";
+import { InputData } from "./TaskTodo";
 
 //create your first component
 const Home = () => {
 	const [content, setContent] = useState([''])
-	
+	console.log(content);
 
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">TODOS</h1>
 				<ul>
 					<InputData />
-					<li> {content} </li>
-					<li> hello </li>
-					<li> hello </li>
-					<li> hello </li>
-					<li> hello </li>	
 				</ul>
 		</div>
 	);
