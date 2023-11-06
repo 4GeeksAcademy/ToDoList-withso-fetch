97c5dc4d44bccbf7c190989dc2825f2aaba05da6
Adrian <142854436+withyso@users.noreply.github.com>
2023-11-06 19:34:40 +0000

    Funcionalidad de borrado added, proyecto completado

--- src/js/component/TaskTodo.jsx
+++ src/js/component/TaskTodo.jsx
@@ -2,10 +2,10 @@ import { object } from "prop-types";
 import React from "react";
 import { useState } from "react";
 
-export const InputData = (content, SetNewTask) => {
+export const InputData = () => {
 
-    const [inputData, SetInputData] = useState('');
-    const [liContent, setliContent] = useState([])
+    const [inputData, setInputData] = useState('');
+    const [liContent, setliContent] = useState([]);
 
     let arrAmmount = liContent.length;
 
@@ -34,7 +34,7 @@ export const InputData = (content, SetNewTask) => {
             }}action="full-form">
                 <input className="input fs-5" 
                     onChange = { (event) => {
-                    SetInputData(event.target.value);
+                    setInputData(event.target.value);
                     }
                 }
                     type ="text"
@@ -45,7 +45,7 @@ export const InputData = (content, SetNewTask) => {
                         <div className="">
                             <i className="fa-solid fa-xmark" onClick={
                                 (event)=>{
-                                    const remainingTask = liContent.filter(newContentli => newContentli.id !== event.target);
+                                    const remainingTask = liContent.filter(newContentli => newContentli.id !== EachliContent.id );
                                     setliContent(remainingTask)
                                     console.log(event.target.value)
                                     console.log(remainingTask)
