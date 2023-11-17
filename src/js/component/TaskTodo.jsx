f8deba06a9416bd6f918205ffee6c06cc02c8cc5
Adrian <142854436+withyso@users.noreply.github.com>
2023-11-17 00:41:30 +0000

    comienzo del proyecto

--- src/js/component/TaskTodo.jsx
+++ src/js/component/TaskTodo.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import { object } from "prop-types";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-export const InputData = () => {
 
+const API_URL = "https://playground.4geeks.com/apis/fake/todos/user"
+export const InputData = () => {
+    
+    const getList = async (URL) => {
+        try{
+            const response = await fetch(URL + '/withyso');
+            if(response.status !== 200){
+                throw new Error(`Ocurrio un error ${response.status}`)
+            }
+            if(response.status !== 304){
+                console.log("necesitamos crear la lista")
+                createList();
+            }
+            const body = await response.json();
+            setTestData([{
+                dataBody: body
+
+            }])
+        } catch(error){
+            console.log(error)
+            return false;
+        }
+    }
+
+    const createList = async (URL) => {
+        try{ 
+            const response = await fetch(URL + '/withyso',{ 
+                method: "POST",
+                body: [],
+                headers: {"Content-Type": "Application/json"}
+            })
+        }catch(error){
+            console.log(error);
+        }
+    }
+    
+    createList();
+   
+
+    const [testData, setTestData] = useState([]);
     const [inputData, setInputData] = useState('');
     const [liContent, setliContent] = useState([]);
     const [isInputEmpty, setIsInputEmpty] = useState(false);
@@ -21,33 +61,26 @@ export const InputData = () => {
         }
     }
 
-    function formSumbit(event){
-        event.preventDefault();
-            if(inputData.trim() === ""){
-                alert('please enter a task')
-                setIsInputEmpty(true);
-            }else{
-                setliContent(
-                    [...liContent, 
-                    {id: crypto.randomUUID(),
-                    content: inputData}
-                    ]);
-                setIsInputEmpty(false);
-                event.target.reset();
-                setInputData("");
-            }  
-    }
-
-    function removeTask(taskId){
-        const remainingTask = liContent.filter(newContentli => newContentli.id !== taskId );
-            setliContent(remainingTask)
-    }
-
     return (
         
         <div className="lista-tareas">
-            <form className="form"   onSubmit ={formSumbit}  
-            
+            <form className="form"   onSubmit ={ (event) => {
+                event.preventDefault();
+                if(inputData.trim() === ""){
+                    alert('please enter a task')
+                    setIsInputEmpty(true);
+                }else{
+                    setliContent(
+                        [...liContent, 
+                        {id: crypto.randomUUID(),
+                        content: inputData}
+                        ]);
+                    console.log(event.target)
+                    setIsInputEmpty(false);
+                    event.target.reset();+
+                    setInputData(""); 
+                }  
+            }}
             action="full-form">
                 <input className={`input fs-5 ${isInputEmpty ? "empty" : ""}`}
                     onChange = { (event) => {
@@ -61,7 +94,14 @@ export const InputData = () => {
                  	{liContent.map(EachliContent =>(
                         <li className="item text-sm-left" key={ EachliContent.id }> { EachliContent.content }  
                         <div className="icon-marker">
-                            <i className="fa-solid fa-xmark" onClick={() =>{removeTask(EachliContent.id)}}></i>
+                            <i className="fa-solid fa-xmark" onClick={
+                                (event)=>{
+                                    const remainingTask = liContent.filter(newContentli => newContentli.id !== EachliContent.id );
+                                    setliContent(remainingTask)
+                                    console.log(event.target.value)
+                                    console.log(remainingTask)
+                                    }
+                                }></i>
                         </div>
                         </li> 
                             )
